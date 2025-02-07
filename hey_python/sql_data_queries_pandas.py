caa3172a75b53212284bb3c15e40c2cc2e11586f
amar8743 <amanemreddy@gmail.com>
2025-02-07 13:27:39 -0800

    adds new script for pandas demo

---
+++ hey_python/sql_data_queries_pandas.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import utils
+import json
+import oracledb
+import warnings
+
+def get_agg_sales(orders_table_name, details_table_name, return_table_name=None):
+    df_orders = get_dataframe(orders_table_name)
+    df_details = get_dataframe(details_table_name)
+
+    df_orders_details = df_orders.merge(df_details)
+
+    df_orders_details['TOTAL'] = df_orders_details.PRICE * df_orders_details.QUANTITY * (1 - df_orders_details.DISCOUNT/100)
+
+    df_orders_details['OFF'] = df_orders_details.PRICE * df_orders_details.QUANTITY * (df_orders_details.DISCOUNT/100)
+
+    df_orders_details = df_orders_details.round(2)
+
+    df_sales = df_orders_details[['ORDATE','EMPL', 'TOTAL', 'OFF']]
+
+    df_date_empl = df_sales.groupby(['ORDATE','EMPL']).sum()
+
+    df_aggs = df_sales.groupby(['ORDATE','EMPL']).agg({'TOTAL': ['sum', 'mean'], 'OFF': 'max'}).round(2)
+
+    df_aggs.columns = df_aggs.columns.map('_'.join).str.strip()
+
+    saveAggSales(df_aggs)
+
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return -1
+
+def saveAggSales(agg_df, return_table_name=None):
+    if return_table_name:
+        con = utils.getconnection()
+        if con is None:
+            print("Failed to establish database connection.")
+            return -1
+        cur = con.cursor()
+        try:
+            # Step 4: Check if the table already exists
+            cur.execute(f"SELECT COUNT(*) FROM all_tables WHERE table_name = UPPER('{return_table_name}')")
+            table_exists = cur.fetchone()[0] > 0
+
+            if table_exists:
+                print(f"Table '{return_table_name}' already exists. Please drop the table or provide a new table name.")
+                return -1
+
+            # Step 5: Create the table if it doesn't exist
+            create_table_query = f"""
+            CREATE TABLE {return_table_name} (
+                or_date VARCHAR2(100),
+                customer VARCHAR2(4000),
+                total_sum NUMBER,
+                total_mean NUMBER,
+                off_max NUMBER
+            )
+            """
+            cur.execute(create_table_query)
+
+            # Step 5.5: Define column order
+            column_order = ["ORDATE", "EMPL", "TOTAL_sum", "TOTAL_mean", "OFF_max"]
+
+            # Step 6: Insert the results into the table
+            for row in agg_df.iterrows()
+                parameter_list = []
+                for col in column_order:
+                    print(f"{col}: {row[col]}: {type(row[col])}")
+                    parameter_list.append(row[col])                    
+
+                insert_query = f"INSERT INTO {return_table_name} (or_date, customer, total_sum, total_mean, off_max) VALUES (:1, :2, :3, :4, :5)"
+                print(tuple(parameter_list))
+                print("\n")
+                #cur.execute(insert_query, tuple(parameter_list))
+
+            # # Step 6: Insert the results into the table
+            # # for operation, result in results.items():
+            # #     insert_query = f"INSERT INTO {return_table_name} (operation, result, ) VALUES (:1, :2)"
+            # #     cur.execute(insert_query, (operation, str(result)))
+
+            # # Commit the transaction
+            # cur.execute("COMMIT")
+
+        except oracledb.DatabaseError as e:
+            print(f"Error working with the table '{return_table_name}': {e}")
+            return -1
+        finally:
+            cur.close()
+
+    # Step 7: Return the results as a JSON object
+    else:
+        return json.dumps(results, default=str)
+
+def get_dataframe(table_name, column_name, operations, return_table_name=None):
+    # Step 1: Establish connection
+    con = utils.getconnection()
+    if con is None:
+        print("Failed to establish database connection.")
+        return -1
+    #time.sleep(1800)
+    try:
+        # Step 2: Query data from the specified table and column
+        query = f"SELECT {column_name} FROM {table_name}"
+        cur = con.cursor()
+        cur.execute(query)
+
+        # Fetch data and create a DataFrame
+        rows = cur.fetchall()
+        #rows = cur.fetch_arrow_all()
+        columns = [col[0] for col in cur.description]  # Get column names
+        # pyarrow_table = pa.Table.from_arrays(rows, names=columns)
+        df = pd.DataFrame(rows, columns=columns)
+
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        return -1
+    finally:
+        cur.close()
