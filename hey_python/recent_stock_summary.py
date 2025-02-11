19d03077560a5db286bb58c001b7cf597f45c7b3
amar8743 <amanemreddy@gmail.com>
2025-02-10 17:03:39 -0800

    Update recent_stock_summary.py

--- hey_python/recent_stock_summary.py
+++ hey_python/recent_stock_summary.py
@@ -12,7 +12,7 @@ def get_data(orders_table_name):
             tkr = yf.Ticker(ticker)
             hist = tkr.history(period='1d')
             hist['Symbol']=ticker
-            stocks = pd.concat([stocks, hist[['Symbol', 'Close']].rename(columns={'Close': 'Price'})])
+            stocks = pd.concat([stocks.dropna(), hist[['Symbol', 'Close']].dropna().rename(columns={'Close': 'Price'})])
 
         stocks.head(10).to_json("demoout.json", orient="records", indent=4)
 
